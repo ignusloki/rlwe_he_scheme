8c087617f47dec3ff40c93ce967b9db1fb571c42
Felipe <felipelimal@outlook.com>
2021-12-05 15:04:28 -0300

    Parte da Alice esta pronta

--- pir2rlwe_alice.py
+++ pir2rlwe_alice.py
@@ -6,43 +6,48 @@ import faulthandler; faulthandler.enable()
 import textwrap
 
 
+#Configuração
+localContextBob = "./imeCoin/alice/req_bob/context_3"
+localChavePKBob = "./imeCoin/alice/req_bob/pub3.key"
+localCPBob = "./imeCoin/alice/req_bob/CP_3"
+localBlocosCP = "./imeCoin/alice/blocos"
+localCP2 = "./imeCoin/alice/resp_bob"
+caminhoBobResp = "./imeCoin/bob/resp_alice"
 
-def splitstring(value):
-    string1, string2 = value[:len(value)//2], value[len(value)//2:]
-    return string1, string2
 
-
-
-#Alice convertendo um bloco de hex para int - Passo 9
+#Alice convertendo um bloco de hex para int da blockchain - Passo 9
 rawBlock = "00000020586dc51cfdb37ad66c8a8e2656375132f759fc008bea42e86ffbd046f80b00001335a52b662f7b40d5c598e5d4cea2276d757e1709ceaa60db89163e616f6ff925549161ffff001f7d5101000101000000010000000000000000000000000000000000000000000000000000000000000000ffffffff1902cd025365787461206d696e65726163616f204e3200000000ffffffff0100f2052a010000001976a914591cbe9f5b9dbaf626bf11b2b3592a11262e051588ac00000000"
 rawBlock = textwrap.wrap(rawBlock, 15)
 
 
 #Alice cifrando um bloco convertido para int criando Vetor CP' - Passo 10 e 11
 he_alice = Pyfhel.Pyfhel()
-localContextBob = "./chaves/context_0"
-localChavePKBob = "./chaves/pub0.key"
 he_alice.restoreContext(localContextBob)
 he_alice.restorepublicKey(localChavePKBob)
 
 
+#Encripta o bloco que foi quebrado em partes menores
 for index in range(len(rawBlock)):
     print("")
     print(str(index) + ": " + str(rawBlock[index]))
     rawBlock[index] = int(rawBlock[index], 16)   
     print(str(index) + ": " + str(rawBlock[index]))
     print("")
-    y = he_alice.encryptInt(rawBlock[index])
-    y.save("./chaves/enc/ctx_01bloco" + str(index))
+    bloco = he_alice.encryptInt(rawBlock[index])
+    bloco.save(localBlocosCP + "/ctx_01bloco" + str(index))
 
 
 #Alice aplicando homomorfismo bloco CP * CP'- Passo 12
 ctxt_restored1 = PyCtxt()
 ctxt_restored2 = PyCtxt()
-ctxt_restored1.load("./chaves/enc/ctx_01bloco0", int)
-ctxt_restored2.load("./chaves/enc/ctx0", int)
-ctxtMul = he_alice.multiply(ctxt_restored1,ctxt_restored2)
 
+for index in range(len(rawBlock)):
+    ctxt_restored1.load(localBlocosCP + "/ctx_01bloco" + str(index), int)
+    ctxt_restored2.load(localCPBob, int)
+    ctxtMul = he_alice.multiply(ctxt_restored1,ctxt_restored2)
+
+    ctxtMul.save(localCP2 + "/CP2_01bloco" + str(index))
+    ctxtMul.save(caminhoBobResp + "/CP2_01bloco" + str(index))
 
 
 
--- pir2rlwe_bob_resp.py
+++ pir2rlwe_bob_resp.py
@@ -7,45 +7,16 @@ import textwrap
 
 
 
-def splitstring(value):
-    string1, string2 = value[:len(value)//2], value[len(value)//2:]
-    return string1, string2
-
-
-
-#Alice convertendo um bloco de hex para int
-rawBlock = "00000020586dc51cfdb37ad66c8a8e2656375132f759fc008bea42e86ffbd046f80b00001335a52b662f7b40d5c598e5d4cea2276d757e1709ceaa60db89163e616f6ff925549161ffff001f7d5101000101000000010000000000000000000000000000000000000000000000000000000000000000ffffffff1902cd025365787461206d696e65726163616f204e3200000000ffffffff0100f2052a010000001976a914591cbe9f5b9dbaf626bf11b2b3592a11262e051588ac00000000"
-rawBlock = textwrap.wrap(rawBlock, 15)
-
-
-#Alice cifrando um bloco convertido para int
-he_alice = Pyfhel.Pyfhel()
-localContextBob = "./chaves/context_0"
-localChavePKBob = "./chaves/pub0.key"
-he_alice.restoreContext(localContextBob)
-he_alice.restorepublicKey(localChavePKBob)
-
-
-for index in range(len(rawBlock)):
-    print("")
-    print(str(index) + ": " + str(rawBlock[index]))
-    rawBlock[index] = int(rawBlock[index], 16)   
-    print(str(index) + ": " + str(rawBlock[index]))
-    print("")
-    y = he_alice.encryptInt(rawBlock[index])
-    y.save("./chaves/enc/ctx_01bloco" + str(index))
-
-
-#Bob recuperanco o bloco fract
+#Bob recuperanco o bloco int
 he_bob = Pyfhel.Pyfhel()
 he_bob.restoreContext(localContextBob)
 he_bob.restorepublicKey(localChavePKBob)
-he_bob.restoresecretKey("./chaves/sec0.key")
+he_bob.restoresecretKey("./imeCoin/bob/chaves/sec3.key")
 ctxt_restored = PyCtxt()
 
 
-ctxt_restored.load("./chaves/enc/ctx_01bloco0", int)
-rawBlock1 = he_bob.decryptInt(ctxt_restored)
+#ctxt_restored.load("./chaves/enc/ctx_01bloco0", int)
+rawBlock1 = he_bob.decryptInt(ctxtMul)
 
 #Coloca a parte do bloco no formato original
 print('{:015x}'.format(rawBlock1))
